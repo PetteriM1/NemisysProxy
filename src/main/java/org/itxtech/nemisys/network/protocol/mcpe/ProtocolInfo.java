eaabd56c8e23a9e97b073967be11853977bf831e
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-08-18 18:43:13 +0300

    Bump version to 1.5

--- src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
+++ src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
@@ -9,10 +9,10 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    int CURRENT_PROTOCOL = Integer.valueOf("161");
+    int CURRENT_PROTOCOL = Integer.valueOf("274");
 
-    String MINECRAFT_VERSION = "v1.4";
-    String MINECRAFT_VERSION_NETWORK = "1.4";
+    String MINECRAFT_VERSION = "v1.5";
+    String MINECRAFT_VERSION_NETWORK = "1.5";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
