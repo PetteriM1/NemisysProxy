cf3aaa98d3e7351c9eb4439ee9a1e1cba767b565
PetteriM1 <petteri.malkavaara1@gmail.com>
2018-10-06 12:28:30 +0300

    Stuff

--- src/main/java/org/itxtech/nemisys/network/protocol/mcpe/AvailableCommandsPacket.java
+++ src/main/java/org/itxtech/nemisys/network/protocol/mcpe/AvailableCommandsPacket.java
@@ -14,25 +14,27 @@ import java.util.stream.Collectors;
      public static final byte NETWORK_ID = ProtocolInfo.AVAILABLE_COMMANDS_PACKET;
      public Map<String, CommandDataVersions> commands;
      public final Map<String, List<String>> softEnums = new HashMap<>();
- 
+
      public static final int ARG_FLAG_VALID = 0x100000;
- 
+
      public static final int ARG_TYPE_INT = 0x01;
      public static final int ARG_TYPE_FLOAT = 0x02;
      public static final int ARG_TYPE_VALUE = 0x03;
      public static final int ARG_TYPE_WILDCARD_INT = 0x04;
-     public static final int ARG_TYPE_OPERATOR = 0x05;
-     public static final int ARG_TYPE_TARGET = 0x06;
-     public static final int ARG_TYPE_STRING = 0x14;
-     public static final int ARG_TYPE_POSITION = 0x1a;
-     public static final int ARG_TYPE_MESSAGE = 0x1c;
-     public static final int ARG_TYPE_TEXT = 0x1f;
-     public static final int ARG_TYPE_JSON = 0x20;
-     public static final int ARG_TYPE_COMMAND = 0x23;
- 
+     public static final int ARG_TYPE_TARGET = 0x05;
+     public static final int ARG_TYPE_WILDCARD_TARGET = 0x06;
+
+     public static final int ARG_TYPE_STRING = 0x0f;
+     public static final int ARG_TYPE_POSITION = 0x10;
+
+     public static final int ARG_TYPE_MESSAGE = 0x13;
+     public static final int ARG_TYPE_RAWTEXT = 0x15;
+     public static final int ARG_TYPE_JSON = 0x18;
+     public static final int ARG_TYPE_COMMAND = 0x1f;
+
      public static final int ARG_FLAG_ENUM = 0x200000;
      public static final int ARG_FLAG_POSTFIX = 0x1000000;
- 
+
      @Override
      public byte pid() {
          return NETWORK_ID;
