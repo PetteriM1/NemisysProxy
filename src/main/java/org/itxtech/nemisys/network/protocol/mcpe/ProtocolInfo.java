1338888296e38f43aeec8a866829b270e6d600cd
Petteri <petteri.malkavaara1@gmail.com>
2019-03-19 17:50:52 +0200

    1.10.0

--- src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
+++ src/main/java/org/itxtech/nemisys/network/protocol/mcpe/ProtocolInfo.java
@@ -6,9 +6,9 @@ package org.itxtech.nemisys.network.protocol.mcpe;
  */
 public interface ProtocolInfo {
 
-    int CURRENT_PROTOCOL = 332;
+    int CURRENT_PROTOCOL = 340;
 
-    String MINECRAFT_VERSION_NETWORK = "1.9.0";
+    String MINECRAFT_VERSION_NETWORK = "1.10.0";
 
     byte LOGIN_PACKET = 0x01;
     byte DISCONNECT_PACKET = 0x05;
